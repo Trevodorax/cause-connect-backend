60a75228f57a26f9cddac4662d9f6414f0fd0237
Paul Gaudeaux <phvcgdx@gmail.com>
2024-03-25 16:08:07 +0100

    add swagger adapter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomExceptionFilter } from './CustomExceptionFilter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new CustomExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cause connect API')
+    .setDescription('API for the cause-connect services')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
