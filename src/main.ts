e74f2d151cb3176fca8b54eb041344d812467579
Paul Gaudeaux <phvcgdx@gmail.com>
2024-03-28 22:16:11 +0100

    setup/enable-cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { CustomExceptionFilter } from './CustomExceptionFilter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new CustomExceptionFilter());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
