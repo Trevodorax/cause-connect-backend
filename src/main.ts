4ae213e7f64d5784e74cd8eb306c91c321720869
Paul Gaudeaux <phvcgdx@gmail.com>
2024-04-16 21:46:52 +0200

    prod(cors): enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new CustomExceptionFilter());
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://www.causeconnect.fr'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3000);
